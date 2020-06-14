5ce40deb397ffe66f27dd9b3edd93a7d76f87e20
Jessiecodes <Jessak1202@gmail.com>
2020-06-14 18:02:39 -0400

    Updates

---
+++ precache-manifest.426a53776da76c366b7ed20a7c056bb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b9bf751fa0e6fbc3cf3e58d07d14f6",
+    "url": "/uxproductpageReact/index.html"
+  },
+  {
+    "revision": "cbfa85a2047bdb499193",
+    "url": "/uxproductpageReact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9ed54b18cfce3bced8b8",
+    "url": "/uxproductpageReact/static/js/2.fa0f7366.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/uxproductpageReact/static/js/2.fa0f7366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfa85a2047bdb499193",
+    "url": "/uxproductpageReact/static/js/main.b1b3f501.chunk.js"
+  },
+  {
+    "revision": "07fc381d1d276dd14964",
+    "url": "/uxproductpageReact/static/js/runtime-main.b7d9b3ec.js"
+  }
+]);
